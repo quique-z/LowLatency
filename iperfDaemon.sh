d4a68d130bf35cfb98af1e245fbb55aa56c58fc1
Manuel Zapata Ruiz <mazapata@microsoft.com>
2020-01-22 12:15:22 -0800

    Fixing Directory

--- iperfDaemon.sh
+++ iperfDaemon.sh
@@ -7,3 +7,20 @@ if [[ $HOSTNAME == server* ]]; then
         /bin/iperf3 -s -D
     fi
 fi
+if [[ $HOSTNAME == client* ]]; then
+        echo 'Client Machine found Test to see if Server is alive'
+        while true; do
+        ANS=$(nc -vn $HOST $PORT </dev/null; echo $?)
+        if [ $ANS == 0 ]; then
+             echo "Success Connecting to $HOST on $PORT"
+             break
+        fi
+        done
+
+        echo 'Run IPERF CLIENT'
+        printf "\033c"
+        echo "Begin Test"
+        sudo iperf3 -u -c $HOST -J --logfile $LOGFILE
+		cd /opt/tools/publish
+		sudo ./ReadIperf $LOGFILE
+fi
